99f7ec7e8233f8809fef86f763f4fc79cc5d1220
韦义烨 <523306566@qq.com>
2022-10-31 22:13:05 +0800

    b plus tree

---
+++ src/main/java/APP.java
@@ -0,0 +1,20 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class APP {
+    public static void main(String[] args) {
+        List<Integer> a =new ArrayList<>(4);
+        a.add(5);
+        a.add(6);
+        a.add(8);
+        a.add(25);
+        System.out.println(Collections.binarySearch(a, 0));
+        System.out.println(Collections.binarySearch(a, 7));
+        System.out.println(Collections.binarySearch(a, 20));
+        System.out.println("------------------");
+        int loc = Collections.binarySearch(a, 4);
+        int childIndex = loc >= 0 ? loc + 1 : -loc - 1;
+        System.out.println(childIndex);
+    }
+}
--- src/main/java/linear/LinkList.java
+++ src/main/java/linear/LinkList.java
@@ -5,73 +5,73 @@ import java.util.Iterator;
 public class LinkList<T> {
 
 
-    Node<T> node;
-    private Node<T> head;
-    private int N;
-
-    public LinkList() {
-    }
-
-    // 清空链表
-    public void clear(){
-        head=null;
-    }
-    // 获取链表的长度
-    public int length(){
-    }
-    // 判断其是否为空
-    public boolean isEmpty(){
-    }
-    //获取指定位置的元素
-    public T get(int i){
-    }
-    // 末尾添加元素
-    public void insert(T t){
-    }
-    // 指定位置添加元素
-    public void insert(int i, T t){
-    }
-    // 删除指定位置的元素
-    public T remove(int i){
-    }
-    // 查找t第一次出现的索引位置
-    public int indexOf(T t){
-    }
-
-    // 单链表反转
-    public void reverse(){
-    }
-
-    public Node reverse(Node curr){
-    }
-
-    private class Node<T> {
-        T item;
-        Node<T> next;
-
-        public Node(T item, Node<T> next) {
-            this.item = item;
-            this.next = next;
-        }
-
-        public Node(T item) {
-            this.item = item;
-            this.next=null;
-        }
-    }
-
-    private class It implements Iterator<T>{
-
-        private Node<T> n;
-
-        @Override
-        public boolean hasNext() {
-            return this.next()==null;
-        }
-
-        @Override
-        public T next() {
-            return ;
-        }
-    }
+//    Node<T> node;
+//    private Node<T> head;
+//    private int N;
+//
+//    public LinkList() {
+//    }
+//
+//    // 清空链表
+//    public void clear(){
+//        head=null;
+//    }
+//    // 获取链表的长度
+//    public int length(){
+//    }
+//    // 判断其是否为空
+//    public boolean isEmpty(){
+//    }
+//    //获取指定位置的元素
+//    public T get(int i){
+//    }
+//    // 末尾添加元素
+//    public void insert(T t){
+//    }
+//    // 指定位置添加元素
+//    public void insert(int i, T t){
+//    }
+//    // 删除指定位置的元素
+//    public T remove(int i){
+//    }
+//    // 查找t第一次出现的索引位置
+//    public int indexOf(T t){
+//    }
+//
+//    // 单链表反转
+//    public void reverse(){
+//    }
+//
+//    public Node reverse(Node curr){
+//    }
+//
+//    private class Node<T> {
+//        T item;
+//        Node<T> next;
+//
+//        public Node(T item, Node<T> next) {
+//            this.item = item;
+//            this.next = next;
+//        }
+//
+//        public Node(T item) {
+//            this.item = item;
+//            this.next=null;
+//        }
+//    }
+//
+//    private class It implements Iterator<T>{
+//
+//        private Node<T> n;
+//
+//        @Override
+//        public boolean hasNext() {
+//            return this.next()==null;
+//        }
+//
+//        @Override
+//        public T next() {
+//            return ;
+//        }
+//    }
 }
